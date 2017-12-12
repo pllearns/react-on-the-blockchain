ec1db019ebf1ec63c2128e794442092412b9e78a
pllearns <pllearns@gmail.com>
2017-12-12 14:46:01 -0800

    Update app with web3 injection, still need to build out InfoDisplay tools

--- src/App.js
+++ src/App.js
@@ -3,29 +3,71 @@ import './App.css';
 import NavBar from './components/NavBar';
 import InfoDisplay from './components/InfoDisplay';
 import Footer from './components/Footer';
+import Web3 from 'web3';
+
+var provider = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+provider.eth.defaultAccount = provider.eth.accounts[0];
+
+var MyContract = provider.eth.contract([{ "constant": true, "inputs": [], "name": "get", "outputs": [{ "name": "", "type": "bytes32" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "x", "type": "bytes32" }], "name": "set", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }]
+);
+
+var contract = MyContract.at('0x3051b4cf6a6b2d776aea5ab7c1020c1fc648195b');
+
+var balance = provider.eth.getBalance(provider.eth.defaultAccount);
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+
     this.state = {
-      message: '',
+      accountNumber: '',
       word: '',
       confirmWord: '',
-      balance: null,
+      accountBalance: null,
       gasPrice: null
     }
-
+    this.storeWord = this.storeWord.bind(this)
 
   }
+
+    storeWord = () => {
+      return this.setState({word: contract.set(this.word)})
+    }
+
+    getWord = () => {
+      return this.state.word
+    }
+
+    getBalance = () => {
+      return this.setState({accountBalance: Number(balance)})
+    }
+
+    getGasPrice = () => {
+      return this.setState({gasPrice: provider.eth.gasPrice})
+    }
+
+    showAccount = () => {
+      return this.setState({ accountBalance: provider.eth.defaultAccount})
+    }
+
+ 
   render() {
     return (
       <div className="App">
         <NavBar />
-        <InfoDisplay />
+        <InfoDisplay 
+          storeWord={this.storeWord} 
+          getWord={this.getWord}
+          getBalance={this.getBalance}
+          getGasPrice={this.getGasPrice}
+          showAccount={this.showAccount} 
+          />
         <Footer />
       </div>
     );
   }
 }
+
 
 export default App;
