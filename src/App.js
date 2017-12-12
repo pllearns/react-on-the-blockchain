264d14272a19ec745ad49dc5ef04c50432bee668
pllearns <pllearns@gmail.com>
2017-12-12 11:38:47 -0800

    Initial commit with starter NavBar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {
+  Nav,
+  NavItem,
+  NavDropdown,
+  MenuItem
+} from 'react-bootstrap';
+
+export default class NavBar extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav bsStyle="tabs" activeKey="1" onSelect={this.handleSelect}>
+          <NavItem eventKey="1" href="/home">React Ethereum Access</NavItem>
+          <NavDropdown eventKey="4" title="Projects" id="nav-dropdown">
+            <MenuItem eventKey="4.1">Accounts</MenuItem>
+            <MenuItem eventKey="4.2">Data</MenuItem>
+            <MenuItem eventKey="4.3">Mission</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey="4.4">Signup/Login</MenuItem>
+          </NavDropdown>
+        </Nav>
+      </div>
+    )
+  }
+}
