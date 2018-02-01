07cf281e603fdeba8ce49d9f5dbbc50907d8ce1a
Sumit patel <sumit.patel93@gmail.com>
2018-02-01 20:36:01 +0530

    Updates on smart contracts
    I have updated the smart contract with some global variable that will, return the 
    -Current Block Number
    -Current Block Difficulty
    -Timestamp 
    -Gas Price for the block

--- contracts/word.sol
+++ contracts/word.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.4.17;
 
 contract Word {
   string public word;
+   uint256 public blocknumber;
+    uint256 public blockdifficulty;
+    uint256 public timestamp;
+    uint256 public Gp;
 
     function Word (string initialWord) public {
         word = initialWord;
@@ -9,6 +13,10 @@ contract Word {
 
     function setWord (string newWord) public {
       word = newWord;
+       blocknumber = block.number;
+        blockdifficulty = block.difficulty;
+        timestamp = block.timestamp;
+        Gp = tx.gasprice;
     }
 
 } 
